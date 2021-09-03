9d6bfe57146920dfa3da830d8c31564e19e9f246
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-09-03 09:58:41 +0530

    Clean boiler template code .

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQbdGV2AeBqA5iRT-aHl2JHm7CU3RXE8Q",
+    authDomain: "whatsapp-clone-d192b.firebaseapp.com",
+    projectId: "whatsapp-clone-d192b",
+    storageBucket: "whatsapp-clone-d192b.appspot.com",
+    messagingSenderId: "103510571920",
+    appId: "1:103510571920:web:01a0aa4b5337c1e32fd115",
+    measurementId: "G-GKCJJ03FWT"
+}; 
